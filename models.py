5271d39811af100986b2d78e695e5389756d59ae
sepehr-glpz <sepehrgolpazir@gmail.com>
2022-08-31 18:08:10 +0430

    deleted extra

--- models.py
+++ models.py
@@ -1,32 +1,60 @@
-# -*- coding: utf-8 -*-
-
 class Payment(models.Model):
-    is_paid = models.BooleanField(default=False)
-    payment_agent = models.CharField(max_length=30)    
+    def __init__(self) -> None:
+        super().__init__()
+        self.is_paid = models.BooleanField(default=False)
+        self.payment_agent = models.CharField(max_length=30)    
 
-    # WHAT?!
+
+    def conclude_payment(self):
+        self.is_paid = True
+
+    def set_payment_agent(self, provider): 
+        self.payment_agent = provider
+
+    def is_payment_open(self):
+        return not self.is_paid
+
+    def check_provider_availability(provider, provider_type = None):
+        if provider_type:
+            return provider.filter(type=provider_type).count()
+        else:
+            provider.count()
+
+
+    payment_agents = {
+        "Provider_1": "Provider1" ,
+        "Provider_2": "AO Provider2",
+        "Provider_3": "Provider 3",
+        "Provider_4": "Complex Provider 4 Name With Surname",
+        "Provider_5": "Provider5 Full Company-Name",
+    }
+
+    def update_payment_agent(self):
+        if self.check_provider_availability(self.provider1, 1):
+            self.set_payment_agent(self.payment_agents["Provider_1"])
+
+        elif self.check_provider_availability(self.provider2, 1):
+            self.set_payment_agent(self.payment_agents["Provider_2"])
+
+        elif self.check_provider_availability(self.provider3, 1):
+            self.set_payment_agent(self.payment_agents["Provider_3"])
+
+        elif self.check_provider_availability(self.provider4):
+            self.set_payment_agent(self.payment_agents["Provider_4"])
+
+        else:
+            self.set_payment_agent(self.payment_agents["Provider_5"])
+
+        self.save()
+
     def get_payment_agent(self):
-        u"""
+        U"""
         A monkey patch to get payment agent. Now is it store in the special field in the database. This method is deprecated and is used for backwards compatibility.
 
         .. deprecated:: r574
         """
-        if not self.is_paid:
-            return u"-"
-
-        if self.payment_agent:
+        if self.is_payment_open():
+            return U"-"
+        else:
             return self.payment_agent
 
-        if self.provider1.filter(type=1).count():
-            self.payment_agent = u"Provider1"
-        elif self.qprovider2.filter(type=1).count():
-            self.payment_agent = u"AO Provider2"
-        elif self.provider3.filter(type=1).count():
-            self.payment_agent = u"Provider3"
-        elif self.provider4.count():
-            self.payment_agent = u"Complex Provider 4 Name With Surname"
-        else:
-            self.payment_agent = u"Provider5 Full Company-Name"
-
-        self.save()
-        return self.payment_agent
